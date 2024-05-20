87e561e7994c2d6c5a320df26357928dee40b760
James Linkous <james.william.linkous@gmail.com>
2024-05-19 23:45:46 -0500

    Timer formatted

--- frontend/src/pages/Timer/Timer.tsx
+++ frontend/src/pages/Timer/Timer.tsx
@@ -13,33 +13,47 @@ const milliseconds = 1000
 
 //accepts duration in milliseconds
 const timeParser = (n: number) => {
-  const seconds = Math.floor((n / 1000) % 60).toString().padStart(2, '0')
-  const minutes = Math.floor((n / 1000 / 60) % 60).toString().padStart(2, '0')
-  return `${minutes}:${seconds}`}
+  const seconds = Math.floor((n / 1000) % 60)
+    .toString()
+    .padStart(2, '0')
+  const minutes = Math.floor((n / 1000 / 60) % 60)
+    .toString()
+    .padStart(2, '0')
+  return `${minutes}:${seconds}`
+}
 
-  const timerReducer = (state: TimerType.TimeState, action: TimerType.TimerReducerAction) => {
-    switch (action.type) {
-      case TimerType.TimerActionEnum.SET:
-        if (action.value) {
-          return {...state, duration: action.value, timer: timeParser(action.value)}
+const timerReducer = (
+  state: TimerType.TimeState,
+  action: TimerType.TimerReducerAction,
+) => {
+  switch (action.type) {
+    case TimerType.TimerActionEnum.SET:
+      if (action.value) {
+        return {
+          ...state,
+          duration: action.value,
+          timer: timeParser(action.value),
         }
-        return state
-      case TimerType.TimerActionEnum.RESET:
-        return initialTimer
-      case TimerType.TimerActionEnum.START:
-        const newDuration = state.duration - milliseconds
-        return ({...state, duration: newDuration, timer: timeParser(newDuration)})
-      default:
-        return state
-    }
+      }
+      return state
+    case TimerType.TimerActionEnum.RESET:
+      return initialTimer
+    case TimerType.TimerActionEnum.START:
+      const newDuration = state.duration - milliseconds
+      return { ...state, duration: newDuration, timer: timeParser(newDuration) }
+    default:
+      return state
   }
+}
 
 export const Timer = () => {
   //Ref is the interval, allowing it to persist between renders
   const Ref = useRef<number | undefined>()
 
-
-  const [newTimer, dispatch] = useReducer<Reducer<any, any>>(timerReducer, initialTimer)
+  const [newTimer, dispatch] = useReducer<Reducer<any, any>>(
+    timerReducer,
+    initialTimer,
+  )
 
   useEffect(() => {
     clearInterval(Ref.current)
@@ -50,29 +64,34 @@ export const Timer = () => {
       <Typography variant="h2">Timer</Typography>
       <TimerKnob />
       <Typography variant="h2">{newTimer.timer}</Typography>
-      {Ref?.current ?
-             <Button variant="tactile" onClick={() => {
-              dispatch({type: TimerType.TimerActionEnum.RESET})
-              clearInterval(Ref.current)
-              Ref.current = undefined
-              }}>
-              Reset
-            </Button>
-      
-      : 
-      <Button variant="tactile" onClick={() => {
-        const id = setInterval(() => {
-          dispatch({type: TimerType.TimerActionEnum.START})
-          }, milliseconds)
-          Ref.current = id
-          }}>
-        Start
-      </Button>
-      }
+      {Ref?.current ? (
+        <Button
+          variant="tactile"
+          onClick={() => {
+            dispatch({ type: TimerType.TimerActionEnum.RESET })
+            clearInterval(Ref.current)
+            Ref.current = undefined
+          }}
+        >
+          Reset
+        </Button>
+      ) : (
+        <Button
+          variant="tactile"
+          onClick={() => {
+            const id = setInterval(() => {
+              dispatch({ type: TimerType.TimerActionEnum.START })
+            }, milliseconds)
+            Ref.current = id
+          }}
+        >
+          Start
+        </Button>
+      )}
       <Button
         variant="tactile"
         onClick={() => {
-          dispatch({type: TimerType.TimerActionEnum.SET, value: 360000})
+          dispatch({ type: TimerType.TimerActionEnum.SET, value: 360000 })
         }}
       >
         + 360000
